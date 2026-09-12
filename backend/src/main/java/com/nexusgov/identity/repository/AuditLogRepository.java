d5d0a24d93182fd25e6710e981ca0f356955e7e1
IT25102993 <it25102993@my.sliit.lk>
2026-09-12 08:45:31 +0530

    Create AuditLogRepository.java

---
+++ backend/src/main/java/com/nexusgov/identity/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.nexusgov.identity.repository;
+
+import com.nexusgov.identity.model.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
