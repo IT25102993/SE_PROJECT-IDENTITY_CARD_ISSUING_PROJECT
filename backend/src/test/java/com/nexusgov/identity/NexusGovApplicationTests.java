5bdb08eb43b1168367acadde78240e691036842b
IT25102993 <it25102993@my.sliit.lk>
2026-09-20 17:04:03 +0530

    Update Files

--- backend/src/test/java/com/nexusgov/identity/NexusGovApplicationTests.java
+++ backend/src/test/java/com/nexusgov/identity/NexusGovApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DATABASE_TO_LOWER=TRUE",
     "spring.datasource.driver-class-name=org.h2.Driver",
     "spring.jpa.hibernate.ddl-auto=create-drop"
 })
