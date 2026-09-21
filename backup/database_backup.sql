173ebd582e69209b4ca8794047b76db901489882
IT25102993 <it25102993@my.sliit.lk>
2026-09-21 11:36:37 +0530

    Update database_backup.sql

--- backup/database_backup.sql
+++ backup/database_backup.sql
@@ -1,6 +1,6 @@
 -- =============================================================
 -- NEXUSGOV IDENTITY ISSUANCE SYSTEM — AUTOMATIC DATABASE BACKUP
--- Generated / Synced: 2026-09-21T06:03:15.623Z
+-- Generated / Synced: 2026-09-21T06:06:07.230Z
 -- Total Applications: 3
 -- =============================================================
 
@@ -45,3 +45,4 @@ SET FOREIGN_KEY_CHECKS = 1;
 -- ── RECENT UPDATE TRANSACTIONS (APPENDED LOG) ───────────────────────
 -- [2026-09-21T05:57:43.157Z] System startup baseline sync
 -- [2026-09-21T06:03:15.625Z] System startup baseline sync
+-- [2026-09-21T06:06:07.230Z] System startup baseline sync
