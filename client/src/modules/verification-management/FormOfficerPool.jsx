b00eed3d7592d0c45ab2354f76cf87c1f9036988
IT25102993 <thilinasakalasooriya@gmail.com>
2026-09-25 10:56:40 +0530

    Create FormOfficerPool.jsx
    Co-Authored-By: Thilina Sakalasooriya <it25102993@my.sliit.lk>

---
+++ client/src/modules/verification-management/FormOfficerPool.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { OfficerDashboard } from './OfficerDashboard';
+
+// Individual job pool page for Form Officers (admin-panel style).
+export const FormOfficerPool = () => {
+  return <OfficerDashboard mode="form" />;
+};
+
+export default FormOfficerPool;
